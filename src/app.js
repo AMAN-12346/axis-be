726ad13dca7bcbbe13bc326522d8344c5dae7ede
Aman <113921809+AMAN-12346@users.noreply.github.com>
2026-05-26 15:29:58 +0530

    Add new origin to CORS middleware

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000", "https://axis-fe-ten.vercel.app","https://www.axissupport.help"],
+  origin: ["http://localhost:3000", "https://axis-fe-ten.vercel.app","https://www.axissupport.help","https://www.axiscreditsupport.in/"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
